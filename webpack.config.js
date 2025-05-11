81d44c8174d5fbcbe096646407d7a9a9bfd8624c
Jonas Laux <jonas.laux@hotmail.com>
2025-05-11 19:05:54 +0200

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
@@ -43,6 +44,17 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env)
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: 'public', 
+          to: '', 
+          globOptions: {
+            ignore: ['**/index.html'] // index.html wird bereits durch HtmlWebpackPlugin verarbeitet
+          }
+        }
+      ]
     }),
   ],
   devServer: {
